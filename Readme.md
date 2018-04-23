dff64813367a7615fb181065e1bb205bc9ea9299
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,8 @@
+# How to: Access the Navigation Dock Panel (in a WinForms application)
+
+
+The complete description is available in the <a href="http://help.devexpress.com/#eXpressAppFramework/CustomDocument114550">How to: Access the Navigation Dock Panel (in a WinForms Application)</a> topic.
+
+<br/>
+
+
---
+++ VB/Solution43.Win/WinApplication.Designer.vb
@@ -0,0 +1,52 @@
+﻿Namespace Solution43.Win
+    Partial Public Class Solution43WindowsFormsApplication
+        ''' <summary> 
+        ''' Required designer variable.
+        ''' </summary>
+        Private components As System.ComponentModel.IContainer = Nothing
+
+        ''' <summary> 
+        ''' Clean up any resources being used.
+        ''' </summary>
+        ''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+            If disposing AndAlso (components IsNot Nothing) Then
+                components.Dispose()
+            End If
+            MyBase.Dispose(disposing)
+        End Sub
+
+        #Region "Component Designer generated code"
+
+        ''' <summary> 
+        ''' Required method for Designer support - do not modify 
+        ''' the contents of this method with the code editor.
+        ''' </summary>
+        Private Sub InitializeComponent()
+            Me.module1 = New DevExpress.ExpressApp.SystemModule.SystemModule()
+            Me.module2 = New DevExpress.ExpressApp.Win.SystemModule.SystemWindowsFormsModule()
+            Me.module3 = New Solution43.Module.Solution43Module()
+            Me.module4 = New Solution43.Module.Win.Solution43WindowsFormsModule()
+            DirectCast(Me, System.ComponentModel.ISupportInitialize).BeginInit()
+            ' 
+            ' Solution43WindowsFormsApplication
+            ' 
+            Me.ApplicationName = "Solution43"
+            Me.Modules.Add(Me.module1)
+            Me.Modules.Add(Me.module2)
+            Me.Modules.Add(Me.module3)
+            Me.Modules.Add(Me.module4)
+            Me.UseOldTemplates = False
+
+            DirectCast(Me, System.ComponentModel.ISupportInitialize).EndInit()
+
+        End Sub
+
+        #End Region
+
+        Private module1 As DevExpress.ExpressApp.SystemModule.SystemModule
+        Private module2 As DevExpress.ExpressApp.Win.SystemModule.SystemWindowsFormsModule
+        Private module3 As Solution43.Module.Solution43Module
+        Private module4 As Solution43.Module.Win.Solution43WindowsFormsModule
+    End Class
+End Namespace
